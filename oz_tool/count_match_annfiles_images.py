d12e5470b15b5663264ed5a765516b9245d02683
5zbro <90824684+OH-JUHYONG@users.noreply.github.com>
2023-11-24 00:34:16 +0900

    annfiles, images count match
    annfiles과 image를 맞춰 줍니다.

--- oz_tool/count_match_annfiles_images.py
+++ oz_tool/count_match_annfiles_images.py
@@ -1,8 +1,11 @@
 import os
 
+
 def limit_files_to_match(directory1, ext1, directory2, ext2, limit):
-    files1 = {f.split('.')[0] for f in os.listdir(directory1) if f.endswith(ext1)}
-    files2 = {f.split('.')[0] for f in os.listdir(directory2) if f.endswith(ext2)}
+    files1 = {f.split('.')[0]
+              for f in os.listdir(directory1) if f.endswith(ext1)}
+    files2 = {f.split('.')[0]
+              for f in os.listdir(directory2) if f.endswith(ext2)}
 
     # 교집합을 찾아서 정렬
     common_files = sorted(list(files1.intersection(files2)))
@@ -17,9 +20,11 @@ def limit_files_to_match(directory1, ext1, directory2, ext2, limit):
             if file not in common_files:
                 os.remove(os.path.join(directory, file + ext))
 
+
 # 파일 경로 설정
 images_test_dir = "/home/ohanthony/datasets/asan/dataset_v8/images/train/"
 annfiles_test_dir = "/home/ohanthony/datasets/asan/dataset_v8/annfiles/train/"
 
 # 파일 제한 함수 호출
-limit_files_to_match(images_test_dir, '.tif', annfiles_test_dir, '.json', 110000)
+limit_files_to_match(images_test_dir, '.tif',
+                     annfiles_test_dir, '.json', 110000)  # 남길 파일 갯수 설정
